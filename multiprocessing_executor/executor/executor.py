09e50bde924ec86df54db103702eb36fd10a3c96
ettore-galli <ettore.galli@gmail.com>
2024-09-22 15:01:37 +0200

    Draft feedback mechanism

--- multiprocessing_executor/executor/executor.py
+++ multiprocessing_executor/executor/executor.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import multiprocessing
 from collections.abc import Callable, Iterable
 from dataclasses import dataclass
+from functools import partial
 from multiprocessing import Queue
 from typing import Any
 
@@ -15,40 +16,77 @@ class Payload:
     pass
 
 
+FeedbackWriter = Callable[[Any], None]
+
 ProcessingWorker = Callable[[Queue], None]
 
 FeedbackWorker = Callable[[Queue], None]
+
+ItemProcessor = Callable[[FeedbackWriter, Any], None]
+
+ItemFeedbackProcessor = Callable[[Any], None]
 
 
 @dataclass(frozen=True)
 class MultiprocessingExecutorProperties:
     workers: int
-    processing_worker: ProcessingWorker
-    feedback_worker: FeedbackWorker
+    item_processor: ItemProcessor
+    item_feedback_processor: ItemFeedbackProcessor
     task_source: Iterable[Any]
+
+
+def processing_worker(
+    queue: Queue, feedback_writer: FeedbackWriter, item_processor: ItemProcessor
+) -> None:
+    while True:
+        item = queue.get()
+        if isinstance(item, PoisonPill):
+            break
+        item_processor(feedback_writer, item)
+
+
+def feedback_worker(
+    queue: Queue, item_feedback_processor: ItemFeedbackProcessor
+) -> None:
+    while True:
+        item = queue.get()
+        if isinstance(item, PoisonPill):
+            break
+        args, kwargs = item
+        item_feedback_processor(*args, **kwargs)
+
+
+def feedback_writer(queue: Queue, *args: tuple, **kwargs: dict) -> None:
+    queue.put((args, kwargs))
 
 
 class MultiprocessingExecutor:
     def __init__(self, properties: MultiprocessingExecutorProperties) -> None:
-        self.properties: MultiprocessingExecutorProperties = properties
 
         self.input_queue: Queue = Queue()
         self.feedback_queue: Queue = Queue()
+        self.feedback_writer = partial(feedback_writer, self.feedback_queue)
+
+        self.properties: MultiprocessingExecutorProperties = properties
 
     def perform(self) -> None:
 
         multiprocessing_context = multiprocessing.get_context("spawn")
 
         feedback_process = multiprocessing_context.Process(
-            target=self.properties.feedback_worker,
-            args=(self.feedback_queue,),
+            target=feedback_worker,
+            args=(self.feedback_queue, self.properties.item_feedback_processor),
         )
         feedback_process.start()
 
         work_processes = [
             multiprocessing_context.Process(
-                target=self.properties.processing_worker,
-                args=(self.input_queue,),
+                target=processing_worker,
+                args=(
+                    self.input_queue,
+                    self.feedback_writer,
+                    self.properties.item_processor,
+                ),
             )
             for _ in range(self.properties.workers)
         ]
--- tests/multiprocessing_executor/test_executor.py
+++ tests/multiprocessing_executor/test_executor.py
@@ -1,37 +1,29 @@
 import os
 from collections.abc import Generator
 from functools import partial
-from multiprocessing import Queue
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from multiprocessing_executor.executor.executor import (
+    FeedbackWriter,
     MultiprocessingExecutor,
     MultiprocessingExecutorProperties,
-    PoisonPill,
 )
 
 
-def proecssing_worker(tempdir: str, queue: Queue) -> None:
-    while True:
-        item = queue.get()
-        if isinstance(item, PoisonPill):
-            break
-        processed_item = "Processed: {item}"
-        outfile = Path(tempdir, item)
-        with Path.open(outfile, "w", encoding="utf-8") as workf:
-            workf.write(processed_item)
+def item_processor(tempdir: str, feedback_writer: FeedbackWriter, item: str) -> None:
+    processed_item = "Processed: {item}"
+    outfile = Path(tempdir, item)
+    with Path.open(outfile, "w", encoding="utf-8") as workf:
+        workf.write(processed_item)
+        feedback_writer(f"processed {item}")
 
 
-def feedback_worker(tempdir: str, queue: Queue) -> None:
-    while True:
-        item = queue.get()
-        if isinstance(item, PoisonPill):
-            break
-        item_feedback = "Feedback of: {item}"
-        outfile = Path(tempdir, f"{item}_feedback")
-        with Path.open(outfile, "w", encoding="utf-8") as feedbackf:
-            feedbackf.write(item_feedback)
+def item_feedback_processor(tempdir: str, *args: tuple, **_: dict) -> None:
+    item_feedback = "Feedback of: {item}"
+    outfile = Path(tempdir, f"{args[0]}_feedback")
+    with Path.open(outfile, "w", encoding="utf-8") as feedbackf:
+        feedbackf.write(item_feedback)
 
 
 def test_multiprocessing_executor_process() -> None:
@@ -43,8 +35,8 @@ def test_multiprocessing_executor_process() -> None:
 
     properties = MultiprocessingExecutorProperties(
         workers=3,
-        processing_worker=partial(proecssing_worker, tmpdir.name),
-        feedback_worker=partial(feedback_worker, tmpdir.name),
+        item_processor=partial(item_processor, tmpdir.name),
+        item_feedback_processor=partial(item_feedback_processor, tmpdir.name),
         task_source=data_source(),
     )
 
@@ -54,6 +46,16 @@ def test_multiprocessing_executor_process() -> None:
     files_list = os.listdir(tmpdir.name)
     assert sorted(files_list) == sorted(
         [
+            "processed test_input_10_feedback",
+            "processed test_input_1_feedback",
+            "processed test_input_2_feedback",
+            "processed test_input_3_feedback",
+            "processed test_input_4_feedback",
+            "processed test_input_5_feedback",
+            "processed test_input_6_feedback",
+            "processed test_input_7_feedback",
+            "processed test_input_8_feedback",
+            "processed test_input_9_feedback",
             "test_input_1",
             "test_input_2",
             "test_input_3",
